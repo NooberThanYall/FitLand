6c5dabce3cd751ad55336c2e636bf90a47898402
shayan <shayanjafarnejad75@gmail.com>
2025-04-21 23:42:20 +0330

    set styles for bottom Menu

--- src/Components/BottomMenu/BottomMenu.tsx
+++ src/Components/BottomMenu/BottomMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './BottomMenu.css'
+import './mediaBottomMenu.css'
 import { CiUser } from "react-icons/ci";
 import { CiShoppingBasket } from "react-icons/ci";
 
@@ -7,10 +8,10 @@ export default function BottomMenu() {
     return (
         <div className='container'>
             <div className="container-bottom-menu">
-                <button className="btn-icons"><CiUser className='icons-bottom-menu' /></button>
-                <button className="btn-icons"><CiUser className='icons-bottom-menu' /></button>
-                <button className="btn-icons"><CiShoppingBasket className='icons-bottom-menu' /></button>
-                <button className="btn-icons"><CiUser className='icons-bottom-menu' /></button>
+                <button className="btn-icons"><CiUser className='icons-bottom-menu' size={30} /></button>
+                <button className="btn-icons"><CiUser className='icons-bottom-menu' size={30} /></button>
+                <button className="btn-icons"><CiShoppingBasket className='icons-bottom-menu' size={30} /></button>
+                <button className="btn-icons"><CiUser className='icons-bottom-menu' size={30} /></button>
             </div>
         </div>
     )
