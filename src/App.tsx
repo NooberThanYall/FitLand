0d4305e340d7dd1d860fd87e57b216d50f99fbf8
shayan <shayanjafarnejad75@gmail.com>
2025-04-22 13:06:29 +0330

    develope html templetes header side right

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import TopBar from './Components/TopBar/TopBar'
 import BottomMenu from './Components/BottomMenu/BottomMenu'
 import NavBar from './Components/NavBar/NavBar'
+import Home from './Pages/Home/Home'
 
 function App() {
 
@@ -9,8 +10,7 @@ function App() {
     <>
     <TopBar />
     <NavBar />
-    
-
+    <Home />
     <BottomMenu />
     </>
   )
