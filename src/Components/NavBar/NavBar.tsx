eff6ef9add7bd2b3670141c97b28e020709c4cf8
shayan <shayanjafarnejad75@gmail.com>
2025-04-22 09:55:26 +0330

    devlope html templetes for nav bar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -3,6 +3,20 @@ import './NavBar.css'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='container'>
+      <div className="container-nav-bar">
+        <div className="container-nav-right">
+          <button className="links-nav-bar">مردانه</button>
+          <button className="links-nav-bar">زنانه</button>
+          <button className="links-nav-bar">بچگانه</button>
+          <button className="links-nav-bar">لوازم ورزشی</button>
+        </div>
+        <div className="container-nav-مثبف">
+          <button className="links-nav-bar">جدید ترین محصولات</button>
+          <button className="links-nav-bar">تخفیفات ویژه</button>
+          <button className="links-nav-bar">پرفروش ترین ها</button>
+        </div>
+      </div>
+    </div>
   )
 }
