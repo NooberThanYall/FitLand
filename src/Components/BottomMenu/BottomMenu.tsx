a3d5fcb198e1b91fe43874dc9503426e656f93c2
shayan <shayanjafarnejad75@gmail.com>
2025-04-22 08:35:57 +0330

    set animation hover for icons and set icons for bottom menu

--- src/Components/BottomMenu/BottomMenu.tsx
+++ src/Components/BottomMenu/BottomMenu.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './BottomMenu.css'
 import './mediaBottomMenu.css'
+import { IoHomeOutline } from "react-icons/io5";
 import { CiUser } from "react-icons/ci";
 import { CiShoppingBasket } from "react-icons/ci";
+import { LiaTshirtSolid } from "react-icons/lia";
 
 export default function BottomMenu() {
     return (
         <div className='container'>
             <div className="container-bottom-menu">
-                <button className="btn-icons"><CiUser className='icons-bottom-menu' size={30} /></button>
-                <button className="btn-icons"><CiUser className='icons-bottom-menu' size={30} /></button>
+                <button className="btn-icons"><LiaTshirtSolid className='icons-bottom-menu' size={30} /></button>
                 <button className="btn-icons"><CiShoppingBasket className='icons-bottom-menu' size={30} /></button>
                 <button className="btn-icons"><CiUser className='icons-bottom-menu' size={30} /></button>
+                <button className="btn-icons"><IoHomeOutline className='icons-bottom-menu' size={30} /></button>
             </div>
         </div>
     )
