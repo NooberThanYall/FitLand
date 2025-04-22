cb112f8ff2919abe55f3c3911bedc2eb0be59fa5
shayan <shayanjafarnejad75@gmail.com>
2025-04-22 10:08:13 +0330

    set styles for nav bar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './NavBar.css'
+import { CiStar } from "react-icons/ci";
+import { AiOutlineThunderbolt } from "react-icons/ai";
+import { TbShoppingCartStar } from "react-icons/tb";
 
 export default function NavBar() {
   return (
@@ -11,10 +14,10 @@ export default function NavBar() {
           <button className="links-nav-bar">بچگانه</button>
           <button className="links-nav-bar">لوازم ورزشی</button>
         </div>
-        <div className="container-nav-مثبف">
-          <button className="links-nav-bar">جدید ترین محصولات</button>
-          <button className="links-nav-bar">تخفیفات ویژه</button>
-          <button className="links-nav-bar">پرفروش ترین ها</button>
+        <div className="container-nav-left">
+          <button className="links-nav-bar"> <CiStar size={18} className='icon-nav-bar'/>جدید ترین محصولات</button>
+          <button className="links-nav-bar"> <AiOutlineThunderbolt size={18} className='icon-nav-bar'/>تخفیفات ویژه</button>
+          <button className="links-nav-bar"> <TbShoppingCartStar size={18} className='icon-nav-bar'/>پرفروش ترین ها</button>
         </div>
       </div>
     </div>
