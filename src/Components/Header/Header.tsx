6b2accb6bf366d56fddec803ee76bb3520ea2e6d
shayan <shayanjafarnejad75@gmail.com>
2025-04-22 13:10:00 +0330

    develope html templetes header side main

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,12 +8,17 @@ export default function Header() {
     return (
         <div className='container'>
             <div className="container-header">
+
                 <div className="container-header-right">
                     <p className="subtitle-header-right">راحت و مطمئن خرید کنید!</p>
                     <h3 className="fisrt-title-header-right">همراه تو در مسیر سلامتی</h3>
                     <h3 className="sec-title-header-right">سهم بزرگ خودتان را امروز بگیرید! </h3>
                     <p className="des-header-right">بزرگترین حراج فصل فیت لند همین حالا شرع کن و محصولات با یه تخفیف شگفت انگیز بخر!</p>
                     <button className='btn-header-right'>مشاهده  محصولات <FaArrowLeft className='icon-btn-header' /></button>
+                </div>
+                
+                <div className="container-header-main">
+                    <img src="Imgs/img-header.png" className='img-header' />
                 </div>
             </div>
         </div>
