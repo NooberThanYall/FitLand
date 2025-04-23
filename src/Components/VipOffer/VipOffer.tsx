058b09e464018402c684a07bde34f5c5dbba858a
shayan <shayanjafarnejad75@gmail.com>
2025-04-23 22:32:54 +0330

    develope html templets vip off

---
+++ src/Components/VipOffer/VipOffer.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './VipOffer.css'
+import './mediaVipOffer.css'
+import { FaArrowLeft } from "react-icons/fa";
+
+export default function VipOffer() {
+    return (
+        <div className='container'>
+            <div className="container-vip-off">
+                
+                <img src="Imgs/backGround-vip-off-cards.png" className='img-back-vip-off' />
+
+                <p className="title-vip-off">تخفیفات ویژه</p>
+                <p className="see-all-btn">مشاهده همه <FaArrowLeft /></p>
+
+                <div className="container-cards-vip-off">
+
+                    <div className="cards-vip-off">
+                        <img src="Imgs/img-1-vip-off.jpg" className='img-vip-off-card' />
+                        <p className="title-product-card-vip-off">سویشرت نایک مدل AO-14</p>
+                        <p className="price-product-card-vip-off">۳۰۰,۰۰۰ تومان</p>
+                        <p className="size-product-card-vip-off">از سایز L تا XXL</p>
+                        <img src="Imgs/colors.png" className='img-color-products' />
+                    </div>
+
+                    {/* <div className="cards-vip-off">
+                        <img src="Imgs/img-2-vip-off.png" className='img-vip-off-card' />
+                        <p className="title-product-card-vip-off">ست نایک مدل S-2000</p>
+                        <p className="price-product-card-vip-off">۲۰۰,۰۰۰ تومان</p>
+                        <p className="size-product-card-vip-off">از سایز L تا XXL</p>
+                        <img src="Imgs/colors.png" className='img-color-products'/>
+                    </div>
+
+                    <div className="cards-vip-off">
+                        <img src="Imgs/img-3-vip-off.png" className='img-vip-off-card' />
+                        <p className="title-product-card-vip-off">کفش نایک مدل S-2000</p>
+                        <p className="price-product-card-vip-off">۲۰۰,۰۰۰ تومان</p>
+                        <p className="size-product-card-vip-off">از سایز L تا XXL</p>
+                        <img src="Imgs/colors.png" className='img-color-products'/>
+                    </div> */}
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import NewBrand from '../../Components/NewBrand/NewBrand'
+import VipOffer from '../../Components/VipOffer/VipOffer'
 
 export default function Home() {
   return (
     <div>
         <Header />
         <NewBrand />
+        <VipOffer />
     </div>
   )
 }
