88005c62eff1f8eb18cb52cb4e50b8081418792d
shayan <shayanjafarnejad75@gmail.com>
2025-04-23 17:13:59 +0330

    set styles for new brand section

--- src/Components/NewBrand/NewBrand.tsx
+++ src/Components/NewBrand/NewBrand.tsx
@@ -9,21 +9,23 @@ export default function NewBrand() {
             <div className="container-card-new-brand">
 
                 <div className="first-card-new-brand">
-                    <img src="Imgs/fisrt-img-new-brand.jpg" className='img-first-card-new-brand' />
+                    <img src="Imgs/fisrt-img-new-brand.jpg" className='img-card-new-brand' />
                     <img src="Imgs/blue-img.png" className='img-blue-new-brand' />
-                    <p>جدید ترین تخفیفات فصل برند پوما</p>
+                    <p className='des-cards'><abbr style={{color: '#FC9877'}}>۵۰ ٪</abbr> تخفیف روی تمام محصولات </p>
+                    <p className='des-cards'>جدید ترین تخفیفات فصل برند پوما</p>
                 </div>
 
                 <div className="sec-card-new-brand">
-                    <img src="Imgs/sec-img-new-brand.jpg" className='img-sec-card-new-brand' />
+                    <img src="Imgs/sec-img-new-brand.jpg" className='img-card-new-brand' />
                     <img src="Imgs/blue-img.png" className='img-blue-new-brand' />
-                    <p>جدید ترین تخفیفات فصل برند نایک</p>
+                    <p className='des-cards'><abbr style={{color: '#FC9877'}}>۵۰ ٪</abbr> تخفیف روی تمام محصولات </p>
+                    <p className='des-cards'>جدید ترین تخفیفات فصل برند نایک</p>
                 </div>
 
                 <div className="container-new-brand-left">
                     <h1 className='title-new-brand-left'>جدید ترین محصولات</h1>
                     <p className="desc-new-brand-left">جدید ترین محصولات با ارسال رایگان تهیه کنید و در سریع ترین زمان درب منزل تحویل بگیرید </p>
-                    <button className="btn-new-brand-left">مشاهده همه محصولات <FaArrowLeft /></button>
+                    <button className="btn-new-brand-left">مشاهده همه محصولات <FaArrowLeft className='icon-btn-new-brand' /></button>
                 </div>
             </div>
         </div>
