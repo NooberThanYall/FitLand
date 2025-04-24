9a78ec70a76f943dac8cb606ba38f5b3ab63f462
shayan <shayanjafarnejad75@gmail.com>
2025-04-24 19:58:12 +0330

    make responsive the newShoes sction

--- src/Components/NewShoes/NewShoes.tsx
+++ src/Components/NewShoes/NewShoes.tsx
@@ -14,23 +14,23 @@ export default function NewShoes() {
                 <div className="container-cards-new-shoes">
 
                     <div className="cards-new-shoes">
-                        <img src="Imgs/img-1-new-shoes.jpg" className='img-new-shoes-card' />
-                        <p className="title-product-card-new-shoes">سویشرت نایک مدل AO-14</p>
+                        <img src="Imgs/img-new-shoes-1.png" className='img-new-shoes-card' />
+                        <p className="title-product-card-new-shoes">نایک مدل AO-14</p>
                         <p className="price-product-card-new-shoes">۳۰۰,۰۰۰ تومان</p>
                         <p className="size-product-card-new-shoes">از سایز L تا XXL</p>
                         <img src="Imgs/colors.png" className='img-color-products-new-shoes' />
                     </div>
 
                     <div className="cards-new-shoes">
-                        <img src="Imgs/img-2-new-shoes.png" className='img-new-shoes-card' />
-                        <p className="title-product-card-new-shoes">ست نایک مدل S-2000</p>
+                        <img src="Imgs/img-new-shoes-2.png" className='img-new-shoes-card' />
+                        <p className="title-product-card-new-shoes">کفش نایک 2024</p>
                         <p className="price-product-card-new-shoes">۲۰۰,۰۰۰ تومان</p>
                         <p className="size-product-card-new-shoes">از سایز L تا XXL</p>
                         <img src="Imgs/colors.png" className='img-color-products-new-shoes' />
                     </div>
 
                     <div className="cards-new-shoes hider-new-shoes">
-                        <img src="Imgs/img-3-new-shoes.png" className='img-new-shoes-card' />
+                        <img src="Imgs/img-new-shoes-3.png" className='img-new-shoes-card' />
                         <p className="title-product-card-new-shoes">کفش نایک مدل S-2000</p>
                         <p className="price-product-card-new-shoes">۲۰۰,۰۰۰ تومان</p>
                         <p className="size-product-card-new-shoes">از سایز L تا XXL</p>
