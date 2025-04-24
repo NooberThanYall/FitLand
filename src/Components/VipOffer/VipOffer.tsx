4011e0d622654d75d037c900f9a125d41a37d676
shayan <shayanjafarnejad75@gmail.com>
2025-04-24 09:09:28 +0330

    set styles for vipOff section

--- src/Components/VipOffer/VipOffer.tsx
+++ src/Components/VipOffer/VipOffer.tsx
@@ -7,11 +7,13 @@ export default function VipOffer() {
     return (
         <div className='container'>
             <div className="container-vip-off">
-                
-                <img src="Imgs/backGround-vip-off-cards.png" className='img-back-vip-off' />
+
+                <div className="container-background">
+                    <img src="Imgs/backGround-vip-off-cards.png" className='img-back-vip-off' />
+                </div>
 
                 <p className="title-vip-off">تخفیفات ویژه</p>
-                <p className="see-all-btn">مشاهده همه <FaArrowLeft /></p>
+                <p className="see-all-btn">مشاهده همه <FaArrowLeft size={15} /></p>
 
                 <div className="container-cards-vip-off">
 
@@ -23,7 +25,7 @@ export default function VipOffer() {
                         <img src="Imgs/colors.png" className='img-color-products' />
                     </div>
 
-                    {/* <div className="cards-vip-off">
+                    <div className="cards-vip-off">
                         <img src="Imgs/img-2-vip-off.png" className='img-vip-off-card' />
                         <p className="title-product-card-vip-off">ست نایک مدل S-2000</p>
                         <p className="price-product-card-vip-off">۲۰۰,۰۰۰ تومان</p>
@@ -37,7 +39,7 @@ export default function VipOffer() {
                         <p className="price-product-card-vip-off">۲۰۰,۰۰۰ تومان</p>
                         <p className="size-product-card-vip-off">از سایز L تا XXL</p>
                         <img src="Imgs/colors.png" className='img-color-products'/>
-                    </div> */}
+                    </div>
 
                 </div>
 
