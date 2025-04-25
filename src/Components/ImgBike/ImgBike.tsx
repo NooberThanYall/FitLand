0769a9f580b7dcdbeef675349f0694ea6a7ff797
shayan <shayanjafarnejad75@gmail.com>
2025-04-25 19:30:57 +0330

    change the bikeImgs style

--- src/Components/ImgBike/ImgBike.tsx
+++ src/Components/ImgBike/ImgBike.tsx
@@ -5,15 +5,19 @@ import './mediaImgBike.css'
 export default function ImgBike() {
   return (
     <div className='container'>
+      <div className="container-img-bike">
 
-        <img src="Imgs/img-bike.png" className='img-bike' />
-        
-      <div className="container-texts-img-bike">
-        <p className="sub-title-img-bike">برای لذت بردن از مسیر</p>
-        <h3 className='title-img-bike'>دوچرخه، اسکیت و اسکوتر بگیر</h3>
-        <button className='btn-img-bike'>مشاهده همه محصولات <FaArrowLeft /></button>
+        <div className="container-texts-img-bike">
+          <p className="sub-title-img-bike">برای لذت بردن از مسیر</p>
+          <h3 className='title-img-bike'>دوچرخه، اسکیت و اسکوتر بگیر</h3>
+          <button className='btn-img-bike'>مشاهده همه محصولات <FaArrowLeft /></button>
+        </div>
+
+        <div className="container-img">
+          <img src="Imgs/img-bike.png" className='img-bike' />
+        </div>
+
       </div>
-
     </div>
   )
 }
