6aa5b30a41c50ee02f9e7c4c07f36868bd4633f3
shayan <shayanjafarnejad75@gmail.com>
2025-04-25 23:05:12 +0330

    set styles for most sell section

--- src/Components/MostSell/MostSell.tsx
+++ src/Components/MostSell/MostSell.tsx
@@ -5,26 +5,26 @@ import { FaArrowLeft } from "react-icons/fa";
 export default function MostSell() {
     return (
         <div className='container'>
-            <div className="container-card-new-brand">
+            <div className="container-card-most-sell">
 
-                <div className="first-card-new-brand">
-                    <img src="Imgs/img-most-sell-1.png" className='img-card-new-brand' />
-                    <img src="Imgs/blue-img.png" className='img-blue-new-brand' />
+                <div className="first-card-most-sell">
+                    <img src="Imgs/img-most-sell-1.png" className='img-card-most-sell' />
+                    <img src="Imgs/blue-img.png" className='img-blue-most-sell' />
                     <p className='des-cards'><abbr style={{ color: '#FC9877' }}>۵۰ ٪</abbr> تخفیف روی تمام محصولات </p>
                     <p className='des-cards'>جدید ترین تخفیفات فصل برند پوما</p>
                 </div>
 
-                <div className="sec-card-new-brand">
-                    <img src="Imgs/img-most-sell-2.png" className='img-card-new-brand' />
-                    <img src="Imgs/blue-img.png" className='img-blue-new-brand' />
+                <div className="sec-card-most-sell">
+                    <img src="Imgs/img-most-sell-2.png" className='img-card-most-sell' />
+                    <img src="Imgs/blue-img.png" className='img-blue-most-sell' />
                     <p className='des-cards'><abbr style={{ color: '#FC9877' }}>۵۰ ٪</abbr> تخفیف روی تمام محصولات </p>
                     <p className='des-cards'>جدید ترین تخفیفات فصل برند نایک</p>
                 </div>
 
-                <div className="container-new-brand-left">
-                    <h1 className='title-new-brand-left'>پر فروش ترین محصولات</h1>
-                    <p className="desc-new-brand-left">پرفروش ترین محصولات با ارسال رایگان تهیه کنید و در سریع ترین زمان درب منزل تحویل بگیرید.</p>
-                    <button className="btn-new-brand-left">مشاهده همه محصولات <FaArrowLeft className='icon-btn-new-brand' /></button>
+                <div className="container-most-sell-left">
+                    <h1 className='title-most-sell-left'>پر فروش ترین محصولات</h1>
+                    <p className="desc-most-sell-left">پرفروش ترین محصولات با ارسال رایگان تهیه کنید و در سریع ترین زمان درب منزل تحویل بگیرید.</p>
+                    <button className="btn-most-sell-left">مشاهده همه محصولات <FaArrowLeft className='icon-btn-most-sell' /></button>
                 </div>
             </div>
         </div>
