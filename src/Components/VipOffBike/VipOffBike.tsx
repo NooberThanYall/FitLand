7b23ce4462d3ba63705cec8f1b1b655956301fe5
shayan <shayanjafarnejad75@gmail.com>
2025-04-25 22:43:49 +0330

    develope html templetes for bikes offer

--- src/Components/VipOffBike/VipOffBike.tsx
+++ src/Components/VipOffBike/VipOffBike.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
 import './VipOffBike.css'
 import './mediaVipOffBike.css'
+import {FaArrowLeft} from 'react-icons/fa'
 
 export default function VipOffBike() {
   return (
-    <div>VipOffBike</div>
+            <div className='container'>
+                <div className="container-new-shoes">
+    
+                    <p className="title-new-shoes">جدید ترین کفش های ورزشی </p>
+                    <p className="see-all-btn-new-shoes">مشاهده همه <FaArrowLeft size={15} /></p>
+    
+                    <div className="container-cards-new-shoes">
+    
+                        <div className="cards-new-shoes">
+                            <img src="Imgs/img-new-shoes-1.png" className='img-new-shoes-card' />
+                            <p className="title-product-card-new-shoes">نایک مدل AO-14</p>
+                            <p className="price-product-card-new-shoes">۳۰۰,۰۰۰ تومان</p>
+                            <p className="size-product-card-new-shoes">از سایز L تا XXL</p>
+                            <img src="Imgs/colors.png" className='img-color-products-new-shoes' />
+                        </div>
+    
+                        <div className="cards-new-shoes">
+                            <img src="Imgs/img-new-shoes-2.png" className='img-new-shoes-card' />
+                            <p className="title-product-card-new-shoes">کفش نایک 2024</p>
+                            <p className="price-product-card-new-shoes">۲۰۰,۰۰۰ تومان</p>
+                            <p className="size-product-card-new-shoes">از سایز L تا XXL</p>
+                            <img src="Imgs/colors.png" className='img-color-products-new-shoes' />
+                        </div>
+    
+                        <div className="cards-new-shoes hider-new-shoes">
+                            <img src="Imgs/img-new-shoes-3.png" className='img-new-shoes-card' />
+                            <p className="title-product-card-new-shoes">کفش نایک مدل S-2000</p>
+                            <p className="price-product-card-new-shoes">۲۰۰,۰۰۰ تومان</p>
+                            <p className="size-product-card-new-shoes">از سایز L تا XXL</p>
+                            <img src="Imgs/colors.png" className='img-color-products-new-shoes' />
+                        </div>
+    
+                    </div>
+    
+                </div>
+            </div>
   )
 }
