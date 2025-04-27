04fb86d316d194aaf031cce6c14d01dbe1bab2ab
shayan <shayanjafarnejad75@gmail.com>
2025-04-27 15:59:29 +0330

    create footer files

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import TopBar from './Components/Global/TopBar/TopBar'
 import BottomMenu from './Components/Global/BottomMenu/BottomMenu'
 import NavBar from './Components/Global/NavBar/NavBar'
 import Home from './Pages/Home/Home'
+import Footer from './Components/Global/Footer/Footer'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <NavBar />
     <Home />
     <BottomMenu />
+    <Footer />
     </>
   )
 }
---
+++ src/Components/Global/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+import './mediaFooter.css'
+
+export default function Footer() {
+    return (
+        <div className='container'>
+            <div className="container-footer">
+                <div className="container-footer-top">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
