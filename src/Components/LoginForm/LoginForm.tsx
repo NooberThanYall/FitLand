5758bd45697fdfe4135842d23c5844a19780dd73
shayan <shayanjafarnejad75@gmail.com>
2025-04-28 08:51:12 +0330

    develope html templets login form

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -3,6 +3,28 @@ import './mediaLoginForm.css'
 
 export default function LoginForm() {
   return (
-    <div>LoginForm</div>
+    <div className="container">
+      <div className="form-login">
+
+        <div className="container-img-login">
+          <img src="Imgs/img-form-1.png" className="img-login" />
+        </div>
+
+        <form className="container-inputs">
+          <h3 className="title-form">ورود</h3>
+
+          <input type="text" className="inputs-login" placeholder="نام و نام خانوادگی" />
+
+          <input type="number" className="inputs-login" placeholder="شماره موبایل" />
+
+          <input type="email" className="inputs-login" placeholder="ایمیل (اختیاری)" />
+
+          <button className="btn-login" type="submit">ورود</button>
+
+          <p className="link-go-to-login"> حساب کاربری ندارید؟ ثبت نام کنید </p>
+        </form>
+
+      </div>
+    </div>
   )
 }
