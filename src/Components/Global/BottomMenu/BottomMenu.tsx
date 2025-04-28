763411fc3b7bbc7708c19f1aa5e3a71125411778
shayan <shayanjafarnejad75@gmail.com>
2025-04-28 10:21:20 +0330

    set route for btn signIn bottom menue

--- src/Components/Global/BottomMenu/BottomMenu.tsx
+++ src/Components/Global/BottomMenu/BottomMenu.tsx
@@ -5,6 +5,7 @@ import { CiUser } from "react-icons/ci";
 import { CiShoppingBasket } from "react-icons/ci";
 import { LiaTshirtSolid } from "react-icons/lia";
 import { MdKeyboardArrowUp } from "react-icons/md";
+import { Link } from 'react-router';
 
 
 export default function BottomMenu() {
@@ -14,7 +15,7 @@ export default function BottomMenu() {
             <MdKeyboardArrowUp className='icon-open-bottom-menu' size={30} />
             <div className='container-bottom-menu'>
                 <button className="btn-icons"><IoHomeOutline className='icons-bottom-menu' size={30} /></button>
-                <button className="btn-icons"><CiUser className='icons-bottom-menu' size={30} /></button>
+                <Link className="btn-icons" to={'/SignIn'}><CiUser className='icons-bottom-menu' size={30} /></Link>
                 <button className="btn-icons"><CiShoppingBasket className='icons-bottom-menu' size={30} /></button>
                 <button className="btn-icons"><LiaTshirtSolid className='icons-bottom-menu' size={30} /></button>
             </div>
