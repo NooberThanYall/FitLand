2c432839563157c3867cd29ffc6769aeb65bee49
shayan <shayanjafarnejad75@gmail.com>
2025-04-28 10:46:07 +0330

    develope the bottom menu logic

--- src/Components/Global/BottomMenu/BottomMenu.tsx
+++ src/Components/Global/BottomMenu/BottomMenu.tsx
@@ -4,18 +4,18 @@ import { IoHomeOutline } from "react-icons/io5";
 import { CiUser } from "react-icons/ci";
 import { CiShoppingBasket } from "react-icons/ci";
 import { LiaTshirtSolid } from "react-icons/lia";
-import { MdKeyboardArrowUp } from "react-icons/md";
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 
 
 export default function BottomMenu() {
 
+    const location = useLocation()
+
     return (
         <div className='container'>
-            <MdKeyboardArrowUp className='icon-open-bottom-menu' size={30} />
             <div className='container-bottom-menu'>
-                <button className="btn-icons"><IoHomeOutline className='icons-bottom-menu' size={30} /></button>
-                <Link className="btn-icons" to={'/SignIn'}><CiUser className='icons-bottom-menu' size={30} /></Link>
+                <Link className="btn-icons" to={'/'}><IoHomeOutline className={`icons-bottom-menu ${location.pathname === '/' ? 'icons-bottom-menu-slected' : ''}`} size={30} /></Link>
+                <Link className="btn-icons" to={'/SignIn'}> <CiUser className={`icons-bottom-menu ${location.pathname === '/SignIn' ? 'icons-bottom-menu-slected' : ''} ${location.pathname === '/Login' ? 'icons-bottom-menu-slected' : ''}`} size={30} /></Link>
                 <button className="btn-icons"><CiShoppingBasket className='icons-bottom-menu' size={30} /></button>
                 <button className="btn-icons"><LiaTshirtSolid className='icons-bottom-menu' size={30} /></button>
             </div>
