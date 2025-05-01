f3836744c649ea56f81a1c6db2782d723ea22cbc
shayan <shayanjafarnejad75@gmail.com>
2025-05-02 01:29:54 +0330

    set styles for shops cards

--- src/Components/CardsShop/CardsShop.tsx
+++ src/Components/CardsShop/CardsShop.tsx
@@ -8,7 +8,8 @@ export default function CardsShop() {
             <p className="title-card-shop">تیشرت زنانه</p>
             <p className="size-card-shop">از سایز M تا 3XL</p>
             <p className="price-card-shop">400,000 تومان</p>
-            <img src="Imgs/colors.png" />
+            <img src="Imgs/colors.png" className='colors-card-shop' />
+            <button className='btn-submit-card-shop'>افزودن به سبد خرید</button>
         </div>
     )
 }
