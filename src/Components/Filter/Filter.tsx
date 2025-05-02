ab582a387d5f877ef65da9d6e3eaff369b5f821c
shayan <shayanjafarnejad75@gmail.com>
2025-05-02 09:56:22 +0330

    develope the filters logic and add btn

--- src/Components/Filter/Filter.tsx
+++ src/Components/Filter/Filter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ToggelBtn from '../ToggelBtn/ToggelBtn'
 import './Filter.css'
 import './mediaFilter.css'
@@ -8,7 +8,13 @@ type isShowFilterType = {
 }
 
 export default function Filter(isShowFilter: isShowFilterType) {
-    
+
+    const [isActive1, setIsActive1] = useState(false)
+    const [isActive2, setIsActive2] = useState(false)
+    const [isActive3, setIsActive3] = useState(false)
+    const [isActive4, setIsActive4] = useState(false)
+    const [isActive5, setIsActive5] = useState(false)
+
     return (
         <div className={`container-filter ${isShowFilter.isShowFilter ? 'active-container-filter' : ''}`}>
 
@@ -30,11 +36,11 @@ export default function Filter(isShowFilter: isShowFilterType) {
             <div className="container-size-filter">
                 <p className="title-size-filter">سایز:</p>
                 <div className="container-btns-sizes">
-                    <button className="btns-size-filter">XXL</button>
-                    <button className="btns-size-filter">XL</button>
-                    <button className="btns-size-filter">L</button>
-                    <button className="btns-size-filter">M</button>
-                    <button className="btns-size-filter">S</button>
+                    <button className={`btns-size-filter ${isActive1 ? 'btns-size-filter-active' : ''}`} onClick={() => setIsActive1(!isActive1)}>XXL</button>
+                    <button className={`btns-size-filter ${isActive2 ? 'btns-size-filter-active' : ''}`} onClick={() => setIsActive2(!isActive2)}>XL</button>
+                    <button className={`btns-size-filter ${isActive3 ? 'btns-size-filter-active' : ''}`} onClick={() => setIsActive3(!isActive3)}>L</button>
+                    <button className={`btns-size-filter ${isActive4 ? 'btns-size-filter-active' : ''}`} onClick={() => setIsActive4(!isActive4)}>M</button>
+                    <button className={`btns-size-filter ${isActive5 ? 'btns-size-filter-active' : ''}`} onClick={() => setIsActive5(!isActive5)}>S</button>
                 </div>
             </div>
 
@@ -42,6 +48,8 @@ export default function Filter(isShowFilter: isShowFilterType) {
                 <p className="title-brand-filter">برند:</p>
                 <input type="text" placeholder='جستجو' className='input-brand-filter' />
             </div>
+
+            <button className='btn-submit-filter'>اعمال فیلترها</button>
 
         </div>
     )
