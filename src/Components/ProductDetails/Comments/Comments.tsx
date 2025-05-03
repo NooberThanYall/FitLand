974ac17c630fabfca59929b8e01cba5cb1f79637
shayan <shayanjafarnejad75@gmail.com>
2025-05-03 19:32:17 +0330

    set styles for comments box

--- src/Components/ProductDetails/Comments/Comments.tsx
+++ src/Components/ProductDetails/Comments/Comments.tsx
@@ -5,7 +5,7 @@ export default function Comments() {
   return (
     <div className="comments-provider">
         <div className="container-name-and-img-user">
-            <img src="Imgs/img-user.png" alt="" />
+            <img src="Imgs/img-user.png" className='img-user' />
             <p className="name-user">شایان</p>
         </div>
         <p className="text-comment">محصولات خیلی خوبی دارید</p>
