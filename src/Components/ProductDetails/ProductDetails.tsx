630847bb7551167f416024a0fe98713ea2426498
shayan <shayanjafarnejad75@gmail.com>
2025-05-03 12:09:36 +0330

    develope the logic like the product

--- src/Components/ProductDetails/ProductDetails.tsx
+++ src/Components/ProductDetails/ProductDetails.tsx
@@ -3,20 +3,22 @@ import './mediaProductDetails.css'
 import { FiPlus } from "react-icons/fi";
 import { FiMinus } from "react-icons/fi";
 import { LuShoppingCart } from "react-icons/lu";
-import { FaRegHeart } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
 import { useState } from 'react';
 
 
 export default function ProductDetails() {
 
-  const [countProduct, setCountProduct] = useState(1)
+  const [countProduct, setCountProduct] = useState<number>(1)
+
+  const [isLiked, setIsLiked] = useState<boolean>(false)
 
   const minusProductCount = () => {
     if (countProduct > 1) {
       setCountProduct(countProduct - 1)
     }
   }
-
+  
   return (
     <div className='container'>
       <div className="container-main-product">
@@ -41,7 +43,7 @@ export default function ProductDetails() {
           <div className="container-add-and-remove-product">
             <div className="plus-minus-product-main-product"><FiPlus style={{ cursor: 'pointer' }} onClick={() => setCountProduct(countProduct + 1)} /> {countProduct} <FiMinus style={{ cursor: 'pointer' }} onClick={minusProductCount} /></div>
             <button className='btn-add-product-to-cart-main-product'>افزودن به سبد خرید <LuShoppingCart size={20} /></button>
-            <FaRegHeart size={20} style={{ color: 'gray', cursor: 'pointer' }} />
+            <FaRegHeart size={20}  onClick={() => setIsLiked(!isLiked)} className={`icon-like-product ${isLiked ? 'like-active' : ''}`} />
           </div>
         </div>
 
