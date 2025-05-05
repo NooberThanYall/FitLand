3bceefd0510b6088b45f1c0c785039fc6732db0d
shayan <shayanjafarnejad75@gmail.com>
2025-05-05 19:28:48 +0330

    develope html templetes baskets card

--- src/Components/CardBasket/CardBasket.tsx
+++ src/Components/CardBasket/CardBasket.tsx
@@ -5,28 +5,19 @@ import { MdDeleteOutline } from "react-icons/md";
 export default function CardBasket() {
 
     return (
-        <div className='card-basket-wrraper'>
-
-            <div className="container-img-basket">
-                <img src="Imgs/img-product.png" className='img-basket' />
+        <div className='basket-card-wraper'>
+            <div className="container-img-and-size-and-count">
+                <img src="Imgs/img-product.png" className='img-card-basket' />
+                <p className="size-product-basket">M</p>
+                <p className="size-product-basket">L</p>
+                <p className="size-product-basket">XXL</p>
+                <p className="count-product">تعداد: 2</p>
             </div>
-
-            <div className="container-details-card-basket">
-                <div className="container-name-and-icon-delete">
-                    <p className="title-card-basket">تیشرت زنانه</p>
-                    <MdDeleteOutline className='icon-delete-card-at-basket' size={25} />
-                </div>
-                <p className="new-price-card-basket">۶۰۰,۰۰۰ تومان</p>
-                <p className="last-price-card-basket">1,200,000</p>
-                <p className="title-size-card-basket">سایز</p>
-                <div className="container-sizes-card-basket">
-                    <p className="sizes-card-basket">M</p>
-                    <p className="sizes-card-basket">L</p>
-                    <p className="sizes-card-basket">XL</p>
-                </div>
-                <p className='title-count-at-basket'>تعداد: 2</p>
+            <div className="container-name-and-price-product-basket">
+                <MdDeleteOutline />
+                <p className="name-product-basket">تیشرت زنانه</p>
+                <p className="price-product-basket">600,000 تومان</p>
             </div>
-
         </div>
     )
 }
