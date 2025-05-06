8d4d4f03024cd02555251d5530e1df2853279b8f
shayan <shayanjafarnejad75@gmail.com>
2025-05-06 09:10:53 +0330

    develope html templets for prices list

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -19,6 +19,15 @@ export default function Cart() {
 
         <div className="container-buy-process">
           <BuyProcess />
+
+          <div className="container-list-prices-basket">
+            <p className="prices-product-basket">قیمت کالا ۲۴۰.۰۰۰ تومان</p>
+            <p className="send-price">هزینه ارسال ۵۰.۰۰۰ تومان</p>
+            <img src="Imgs/Line-modal.png" className='line-prices-list-basket' />
+            <p className="total-price-basket">مبلغ قابل پرداخت ۲۹۰.۰۰۰ تومان</p>
+            <button className='btn-submit-pruches'>تکمیل فرایند خرید</button>
+          </div>
+
         </div>
 
       </div>
