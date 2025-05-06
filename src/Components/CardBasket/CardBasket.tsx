d35a230cde0958556cfb14e16ebaae289097425e
shayan <shayanjafarnejad75@gmail.com>
2025-05-06 14:38:15 +0330

    set the cards and the prices list

--- src/Components/CardBasket/CardBasket.tsx
+++ src/Components/CardBasket/CardBasket.tsx
@@ -7,19 +7,13 @@ export default function CardBasket() {
     return (
         <div className='basket-card-wraper'>
             <img src="Imgs/img-product.png" className='img-card-basket' />
-
-            <div className="container-product-info-basket">
-                <p className="name-product-basket">تیشرت زنانه</p>
-                <p className="price-product-basket">600,000 تومان</p>
-                <p className="count-product">تعداد: 2</p>
-                <div className="container-sizes">
-                    <p className="size-product-basket">M</p>
-                    <p className="size-product-basket">L</p>
-                    <p className="size-product-basket">XXL</p>
-                </div>
+            <p className="name-product-basket">تیشرت زنانه</p>
+            <p className="price-product-basket">600,000 تومان</p>
+            <div className="container-sie-and-count-product-in-basket">
+                <p className="count-product">تعداد: <abbr style={{fontWeight: '700'}}>2</abbr></p>
+                <p className="size-product-basket">سایز: <abbr style={{fontWeight: '700'}}>M</abbr></p>
                 <MdDeleteOutline className='icon-remove-product-from-basket' size={25} />
             </div>
-
         </div>
     )
 }
--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -8,26 +8,27 @@ export default function Cart() {
 
   return (
     <div className='container'>
+
+      <BuyProcess />
+
       <p className='title-basket'>سبد خرید</p>
-      <div className="container-basket">
+      <div className="container-cards-and-prices-list">
 
         <div className="container-cards-in-shop">
           <CardBasket />
           <CardBasket />
           <CardBasket />
+          <CardBasket />
+          <CardBasket />
+          <CardBasket />
         </div>
 
-        <div className="container-buy-process">
-          <BuyProcess />
-
-          <div className="container-list-prices-basket">
-            <p className="prices-product-basket">قیمت کالا ۲۴۰.۰۰۰ تومان</p>
-            <p className="send-price">هزینه ارسال ۵۰.۰۰۰ تومان</p>
-            <img src="Imgs/Line-modal.png" className='line-prices-list-basket' />
-            <p className="total-price-basket">مبلغ قابل پرداخت ۲۹۰.۰۰۰ تومان</p>
-            <button className='btn-submit-pruches'>تکمیل فرایند خرید</button>
-          </div>
-
+        <div className="container-list-prices-basket">
+          <p className="prices-product-basket"><abbr style={{ fontWeight: '500' }}>قیمت کالا</abbr> ۲۴۰.۰۰۰ تومان</p>
+          <p className="send-price"><abbr style={{ fontWeight: '500' }}>هزینه ارسال</abbr> ۵۰.۰۰۰ تومان</p>
+          <img src="Imgs/Line-modal.png" className='line-prices-list-basket' />
+          <p className="total-price-basket"><abbr style={{ fontWeight: '500' }}>مبلغ قابل پرداخت</abbr> ۲۹۰.۰۰۰ تومان</p>
+          <button className='btn-submit-pruches'>تکمیل فرایند خرید</button>
         </div>
 
       </div>
