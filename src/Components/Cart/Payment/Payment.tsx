080081728315c00a163e958e598094f6f04e7c29
shayan <shayanjafarnejad75@gmail.com>
2025-05-07 18:46:20 +0330

    set styles for user address

--- src/Components/Cart/Payment/Payment.tsx
+++ src/Components/Cart/Payment/Payment.tsx
@@ -9,19 +9,22 @@ export default function Payment() {
 
                 <p className="title-invoice">فاکتور محصولات</p>
                 <div className="container-invoice">
-
                     <CardsInvoice />
                     <CardsInvoice />
                     <CardsInvoice />
-
                 </div>
 
                 <div className="contaienr-user-address">
-                    <p className="main-address">بجنورد, میدان شهید, بین شهید و 17 شهریور ,کوچه عزیز مصر ,پلاک 13 </p>
-                    <p className="users-city">خراسان شمالی - بجنورد</p>
-                    <p className="users-phone-number">09149582599</p>
-                    <p className="users-name">شایان جعفرنژاد</p>
+                    <img src="Imgs/img-map.png" className='img-map' />
+                    <div className="container-detail-address">
+                        <p className="title-user-address">آدرس شما</p>
+                        <p className="main-address">کوردستان, مهاباد,خیابان آزادی</p>
+                        <p className="users-city">کوردستان - مهاباد</p>
+                        <p className="users-phone-number">09149582599</p>
+                        <p className="users-name">شایان جعفرنژاد</p>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
