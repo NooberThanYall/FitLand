a9a5bdbb3714afcac11d60e2f8c398fd3a61eadc
shayan <shayanjafarnejad75@gmail.com>
2025-05-10 11:23:30 +0330

    make responsvie the 404 page

--- src/Components/404/Page404.tsx
+++ src/Components/404/Page404.tsx
@@ -8,7 +8,7 @@ export default function Page404() {
     <div className='container'>
       <div className="container-page-404">
         <img src="Imgs/img-404.png" className='img-page-404' />
-        <p className="text-page-404">متاسفانه صفحه مورد نظر پیدا نشد :(</p>
+        <p className="text-page-404">متاسفانه صفحه مورد نظر پیدا نشد</p>
         <Link className="btn-page-404" to={'/'}>صفحه اصلی <FiHome size={20} /></Link>
       </div>
     </div>
