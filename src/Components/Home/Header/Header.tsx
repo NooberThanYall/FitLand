3588190b18a26230b9ff0c0861fc5959b54e3d43
shayan <shayanjafarnejad75@gmail.com>
2025-05-10 11:44:37 +0330

    set rout for btn header

--- src/Components/Home/Header/Header.tsx
+++ src/Components/Home/Header/Header.tsx
@@ -4,6 +4,7 @@ import { FaArrowLeft } from "react-icons/fa";
 import { BsShopWindow } from "react-icons/bs";
 import { AiOutlineLike } from "react-icons/ai";
 import { MdOutlineDateRange } from "react-icons/md";
+import { Link } from 'react-router';
 
 
 export default function Header() {
@@ -16,7 +17,7 @@ export default function Header() {
                     <h3 className="fisrt-title-header-right">همراه تو در مسیر سلامتی</h3>
                     <h3 className="sec-title-header-right">سهم بزرگ خودتان را امروز بگیرید! </h3>
                     <p className="des-header-right">بزرگترین حراج فصل فیت لند همین حالا شرع کن و محصولات با یه تخفیف شگفت انگیز بخر!</p>
-                    <button className='btn-header-right'>مشاهده  محصولات <FaArrowLeft className='icon-btn-header' /></button>
+                    <Link className='btn-header-right' to={'/Shop'}>مشاهده  محصولات <FaArrowLeft className='icon-btn-header' /></Link>
                 </div>
 
                 <div className="container-header-main">
