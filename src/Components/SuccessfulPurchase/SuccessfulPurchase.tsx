8919e915aee9e6f189f60911226ffaef1dee9489
shayan <shayanjafarnejad75@gmail.com>
2025-05-10 10:52:19 +0330

    make responsive the SuccessfulPurchase page

--- src/Components/SuccessfulPurchase/SuccessfulPurchase.tsx
+++ src/Components/SuccessfulPurchase/SuccessfulPurchase.tsx
@@ -10,14 +10,14 @@ export default function SuccessfulPurchase() {
         <div className="container-SuccessfulPurchase">
 
             <div className="container-texts-SuccessfulPurchase">
-                <p className="title-SuccessfulPurchase">سفارش شما با موفقیت انجام شد <TiTick size={60} /></p>
+                <p className="title-SuccessfulPurchase">سفارش شما با موفقیت انجام شد</p>
                 <p className="desc-SuccessfulPurchase">در تاریخ معین شده منتظر سفارش خود باشید!</p>
                 <p className="buy-code-SuccessfulPurchase">کد رهگیری سفارش: 045162516</p>
                 <Link className="btn-SuccessfulPurchase" to={'/'}>صفحه اصلی <FiHome size={20} /></Link>
             </div>
 
             <div className="container-img-SuccessfulPurchase">
-                <img src="Imgs/img-SuccessfulPurchase.png" alt="" />
+                <img src="Imgs/img-SuccessfulPurchase.png" className='img-SuccessfulPurchase' />
             </div>
         </div>
     </div>
