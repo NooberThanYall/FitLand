213f96530ea58266e62e8d80ae9be542acdbbb14
shayan <shayanjafarnejad75@gmail.com>
2025-05-10 10:33:36 +0330

    set styles for SuccessfulPurchase page

--- src/Components/SuccessfulPurchase/SuccessfulPurchase.tsx
+++ src/Components/SuccessfulPurchase/SuccessfulPurchase.tsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router';
 import './SuccessfulPurchase.css'
 import './mediaSuccessfulPurchase.css'
 import { FiHome } from "react-icons/fi";
+import { TiTick } from "react-icons/ti";
 
 export default function SuccessfulPurchase() {
   return (
@@ -8,10 +10,10 @@ export default function SuccessfulPurchase() {
         <div className="container-SuccessfulPurchase">
 
             <div className="container-texts-SuccessfulPurchase">
-                <p className="title-SuccessfulPurchase">سفارش شما با موفقیت انجام شد</p>
+                <p className="title-SuccessfulPurchase">سفارش شما با موفقیت انجام شد <TiTick size={60} /></p>
                 <p className="desc-SuccessfulPurchase">در تاریخ معین شده منتظر سفارش خود باشید!</p>
                 <p className="buy-code-SuccessfulPurchase">کد رهگیری سفارش: 045162516</p>
-                <button className="btn-SuccessfulPurchase">صفحه اصلی <FiHome /></button>
+                <Link className="btn-SuccessfulPurchase" to={'/'}>صفحه اصلی <FiHome size={20} /></Link>
             </div>
 
             <div className="container-img-SuccessfulPurchase">
